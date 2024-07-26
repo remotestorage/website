b13a8315b9273725332eacd4e8f5381e992db6de
Râu Cao <raucao@kosmos.org>
2024-07-26 13:18:11 +0200

    Update build

--- dist/assets/README.md.IUj29eG1.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a,a5 as o}from"./chunks/framework.CTkMpXT2.js";const b=JSON.parse('{"title":"remotestorage.io","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),r={name:"README.md"},n=o("",12),s=[n];function l(i,h,d,c,p,u){return a(),t("div",null,s)}const g=e(r,[["render",l]]);export{b as __pageData,g as default};
